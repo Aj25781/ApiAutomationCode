9898438aed79bcd6320a2de9af64c7ba5d38a800
Aj25781 <ajay25781@gmail.com>
2024-11-07 22:18:19 +0530

    first commit to api code

---
+++ src/test/java/files/bodyPayload.java
@@ -0,0 +1,24 @@
+package files;
+
+public class bodyPayload {
+	
+	public static String payload() {
+		return"{\r\n"
+				+ "  \"location\": {\r\n"
+				+ "    \"lat\": -38.383494,\r\n"
+				+ "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n"
+				+ "  \"accuracy\": 50,\r\n"
+				+ "  \"name\": \"Frontline house\",\r\n"
+				+ "  \"phone_number\": \"9459193601\",\r\n"
+				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+				+ "  \"types\": [\r\n"
+				+ "    \"shoe park\",\r\n"
+				+ "    \"shop\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"website\": \"http://google.com\",\r\n"
+				+ "  \"language\": \"French-IN\"\r\n"
+				+ "}";
+	}
+
+}
